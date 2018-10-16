38dd76951796aca92e365b12eb9ed5bf573a6e06
adolphchris19 <adolphchris19@gmail.com>
2018-10-16 14:11:20 -0700

    added sports section

--- app/src/main/java/com/adolphchris/android/newshour/Adapter/NewsFeedAdapter.java
+++ app/src/main/java/com/adolphchris/android/newshour/Adapter/NewsFeedAdapter.java
@@ -1,4 +1,76 @@
 package com.adolphchris.android.newshour.Adapter;
 
-public class NewsFeedAdapter {
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.adolphchris.android.newshour.R;
+import com.adolphchris.android.newshour.UI.NewsFeed;
+import com.squareup.picasso.Picasso;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
+
+public class NewsFeedAdapter extends ArrayAdapter<NewsFeed> {
+
+    private static final String TAG = NewsFeedAdapter.class.getSimpleName();
+
+    public NewsFeedAdapter(Context context, ArrayList<NewsFeed> newsFeed) {
+        super(context, 0, newsFeed);
+    }
+
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        View listItemView = convertView;
+
+        if (listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_item, parent, false);
+        }
+        NewsFeed currentNewsFeed = getItem(position);
+
+        ImageView articleImage = listItemView.findViewById(R.id.article_image);
+        Picasso.get()
+                .load(currentNewsFeed.getImageUrl())
+                .placeholder(R.drawable.ic_launcher_background)
+                .into(articleImage);
+
+        TextView articleTitle = listItemView.findViewById(R.id.article_title);
+        articleTitle.setText(currentNewsFeed.getHeadLine());
+
+        TextView authorName = listItemView.findViewById(R.id.author_name);
+        authorName.setText(currentNewsFeed.getAuthorName());
+
+
+        TextView articleDate = listItemView.findViewById(R.id.article_date);
+        articleDate.setText(formatDate(currentNewsFeed.getPublicationDate()));
+
+        return listItemView;
+    }
+
+    private static String formatDate(String dataObject) {
+        String jsonDatePattern = "yyyy-MM-dd'T'HH:mm:ss'Z'";
+        SimpleDateFormat jsonFormatter = new SimpleDateFormat(jsonDatePattern, Locale.US);
+        try {
+            Date parsedJsonDate = jsonFormatter.parse(dataObject);
+            String finalDatePattern = "EEE, MMM d, ''yyyy, h:mm a";
+            SimpleDateFormat finalDateFormatter = new SimpleDateFormat(finalDatePattern, Locale.US);
+            return finalDateFormatter.format(parsedJsonDate);
+        } catch (ParseException e) {
+            Log.e(TAG, "Error parsing JSON date: ", e);
+            return "Error";
+        }
+    }
 }
+
--- app/src/main/java/com/adolphchris/android/newshour/UI/NewsFeed.java
+++ app/src/main/java/com/adolphchris/android/newshour/UI/NewsFeed.java
@@ -1,4 +1,40 @@
 package com.adolphchris.android.newshour.UI;
 
 public class NewsFeed {
+
+    private String mImageUrl;
+    private String mHeadLine;
+    private String mPublicationDate;
+    private String mUrl;
+    private String mAuthorName;
+
+    public NewsFeed(String mImageUrl, String mHeadLine, String mPublicationDate, String mAuthorName, String mUrl) {
+        this.mImageUrl = mImageUrl;
+        this.mHeadLine = mHeadLine;
+        this.mPublicationDate = mPublicationDate;
+        this.mUrl = mUrl;
+        this.mAuthorName = mAuthorName;
+    }
+
+    public String getImageUrl() {
+        return mImageUrl;
+    }
+
+    public String getHeadLine() {
+        return mHeadLine;
+    }
+
+    public String getPublicationDate() {
+        return mPublicationDate;
+    }
+
+    public String getAuthorName() {
+        return mAuthorName;
+    }
+
+    public String getUrl() {
+        return mUrl;
+    }
 }
+
+
