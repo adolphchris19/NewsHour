9168fdbe60ba1042cc5e397cf004146e7441dfbe
adolphchris19 <adolphchris19@gmail.com>
2018-10-17 00:20:26 -0700

    Added the science and world section, on to the next part

--- app/src/main/java/com/adolphchris/android/newshour/Loaders/ScienceLoader.java
+++ app/src/main/java/com/adolphchris/android/newshour/Loaders/ScienceLoader.java
@@ -1,4 +1,46 @@
 package com.adolphchris.android.newshour.Loaders;
 
-public class ScienceLoader {
+import android.content.Context;
+import android.support.v4.content.AsyncTaskLoader;
+import android.util.Log;
+
+import com.adolphchris.android.newshour.UI.NewsFeed;
+import com.adolphchris.android.newshour.Utils.HomeUtils;
+import com.adolphchris.android.newshour.Utils.ScienceUtils;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+
+public class ScienceLoader extends AsyncTaskLoader<List<NewsFeed>> {
+
+    public static final String LOG_TAG = ScienceLoader.class.getSimpleName();
+
+    public ScienceLoader(Context context) {
+        super(context);
+        Log.i(LOG_TAG, "THIS: scienceLoader constructor is called");
+    }
+
+    @Override
+    protected void onStartLoading() {
+        Log.i(LOG_TAG, "THIS: ScienceLoader onStartLoading is called");
+        forceLoad();
+    }
+
+    @Override
+    public List<NewsFeed> loadInBackground() {
+        Log.i(LOG_TAG, "THIS: ScienceLoader loadInBackground is called");
+        Log.i(LOG_TAG, "TEST:  fetchNewsFeed called");
+        URL url = ScienceUtils.createUrl();
+        String jsonResponse = null;
+
+        try {
+            jsonResponse = ScienceUtils.makeHttpConnection(url);
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "fetchNewsFeed: problem fetchNewsFeed", e);
+        }
+        List<NewsFeed> newsFeeds = ScienceUtils.extractFeatureFromJson(jsonResponse);
+        return newsFeeds;
+    }
 }
+
--- app/src/main/java/com/adolphchris/android/newshour/Loaders/WorldLoader.java
+++ app/src/main/java/com/adolphchris/android/newshour/Loaders/WorldLoader.java
@@ -1,4 +1,46 @@
 package com.adolphchris.android.newshour.Loaders;
 
-public class WorldLoader {
+import android.content.Context;
+import android.support.v4.content.AsyncTaskLoader;
+import android.util.Log;
+
+import com.adolphchris.android.newshour.UI.NewsFeed;
+import com.adolphchris.android.newshour.Utils.HomeUtils;
+import com.adolphchris.android.newshour.Utils.WorldUtils;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+
+public class WorldLoader extends AsyncTaskLoader<List<NewsFeed>> {
+
+    public static final String LOG_TAG = WorldLoader.class.getSimpleName();
+
+    public WorldLoader(Context context) {
+        super(context);
+        Log.i(LOG_TAG, "THIS: WorldLoader constructor is called");
+    }
+
+    @Override
+    protected void onStartLoading() {
+        Log.i(LOG_TAG, "THIS: WorldLoader onStartLoading is called");
+        forceLoad();
+    }
+
+    @Override
+    public List<NewsFeed> loadInBackground() {
+        Log.i(LOG_TAG, "THIS: WorldLoader loadInBackground is called");
+        Log.i(LOG_TAG, "TEST:  fetchNewsFeed called");
+        URL url = WorldUtils.createUrl();
+        String jsonResponse = null;
+
+        try {
+            jsonResponse = WorldUtils.makeHttpConnection(url);
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "fetchNewsFeed: problem fetchNewsFeed", e);
+        }
+        List<NewsFeed> newsFeeds = WorldUtils.extractFeatureFromJson(jsonResponse);
+        return newsFeeds;
+    }
 }
+
